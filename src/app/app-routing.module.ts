23ffeec425fd460aa98eba40233408d66e26935f
bddiakite <ffffffffff>
2022-09-09 11:31:40 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: "",
+  redirectTo:"/tirageA",
+  pathMatch: 'full'
+},
+{
+  path: "tirageA", component: AccueilComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
